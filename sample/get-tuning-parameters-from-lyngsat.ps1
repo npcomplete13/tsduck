28f61f91ef7a13cb42efaac036284732604329fc
Vivek Unune <npcomplete13@gmail.com>
2018-12-19 17:20:26 -0500

    Fix Freq

--- sample/get-tuning-parameters-from-lyngsat.ps1
+++ sample/get-tuning-parameters-from-lyngsat.ps1
@@ -160,7 +160,7 @@ function ParseLyngSat([string] $url, [string] $outFile)
                 $line = "--frequency ${freq}000000 --polarity $polarity --symbol-rate ${symbols}000 --fec $fec --delivery $system --modulation $modulation"
                 $output += "[CHANNEL]"
                 $output += "        DELIVERY_SYSTEM = DVBS"
-                $output += "        FREQUENCY = ${freq}000000"
+                $output += "        FREQUENCY = ${freq}000"
                 $output += "        POLARIZATION = $polarity"
                 $output += "        SYMBOL_RATE = ${symbols}000"
                 $output += "        INNER_FEC = $fec"
